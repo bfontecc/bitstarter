5cd26a3a888f98c439c633c5ca096f89da3ea1c4
Bret Fontecchio <bret.fontecchio@gmail.com>
2013-07-12 03:24:37 +0000

    fixed little html bug

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express 	= require('express');
 var fs 		= require('fs');
-var app 	= express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
     var body_view = fs.readFileSync('./index.html', 'ascii' ,function(err, data) {
